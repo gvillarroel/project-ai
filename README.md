e61707a7d23addf56328300c57d6c7dc00284b57
Gerardo Villarroel <villarroel.gj@gmail.com>
2021-09-24 18:18:37 -0300

    upadting economicos spider

--- README.md
+++ README.md
@@ -4,7 +4,15 @@
 
 Ejecutar spider
 ```bash
-scrapy runspider spiders/preremates.py -O data/preremates.json
+scrapy runspider spiders/preremates.py -O data/preremates.json --loglevel=ERROR
 
-scrapy runspider spiders/economicos.py -O data/economicos.json
+scrapy runspider spiders/economicos.py -O data/economicos.json --loglevel=ERROR
+
+
+scrapy runspider spiders/yapo.py -O data/yapo.json --loglevel=ERROR
+
+scrapy runspider spiders/yapo_chile.py -O data/yapo_chile.json --loglevel=ERROR
+Get-Content .\yapo_chile.json -Tail 1
+Select-String -path .\yapo_chile.json -pattern "PATIO, 2 ESTACIONAMIENTOS y 1 BODEGA muy amplios."
+Get-ChildItem data/economicos.json
 ```
--- spiders/economicos.py
+++ spiders/economicos.py
@@ -1,22 +1,52 @@
 import unicodedata
+from numpy import extract
 import scrapy
 import time
+import logging
+import re
+import json
+import pandas as pd
+import os
+logger = logging.getLogger()
+import re
 
 class ElectronicsSpider(scrapy.Spider):
-    name = "electronics"
+    name = "economicos"
     allowed_domains = ["www.economicos.cl"]
     start_urls = [
-        'https://www.economicos.cl/todo_chile/remates_de_propiedades_el_mercurio'
+        'https://www.economicos.cl/todo_chile/propiedades'
     ]
 
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name=name, **kwargs)
+        try:
+            self.memory = pd.read_json(open(f"data/{self.name}_memory.json"))
+        except:
+            self.memory = pd.DataFrame.from_dict({"url": ["https://www.yapo.cl/chile/comprar?ca=12_s&l=0"]})
+        print(self.memory.head())
     def parse(self, response):
-        time.sleep(2)
+        time.sleep(1)
+        print(f"Processing {response.url}")
+        detalles = response.css("#detalle")
+        got_link = False
         for link in response.css("a::attr(href)"):
             l = str(link.extract())
-            if l.startswith("/todo_chile/remates_de_propiedades_el_mercurio")\
-                    or l.startswith("/remates/"):
-                yield response.follow( l, self.parse )
-        for detalle in response.css("#detalle"):
-            desc = unicodedata.normalize('NFKD', u"\n".join(detalle.css("#description p::text").extract()))
-            date = u"\n".join([d for d in detalle.css("#specs li::text").extract()])
-            yield {"url":str(response.url), "desc": desc, "date":date}
+            if l.startswith("/propiedades") or "propiedades?" in l:
+                if not detalles and self.memory[self.memory.url.str.contains(l) ].count()[0] == 0:
+                    got_link = True
+                    if l.startswith("/propiedades"):
+                        self.memory = self.memory.append({"url": l}, ignore_index=True)
+                    yield response.follow( l, self.parse )
+        for detalle in detalles:
+            data = {
+                "url": response.url,
+                "description": unicodedata.normalize('NFKD', u" ".join(detalle.css("#description p::text").extract())),
+                "price": " ".join(map(str.strip, detalle.css("div.cont_price_detalle_f::text").extract()))
+                }
+            data = {**data, **dict([(" ".join(d.css("span::text").extract()), " ".join(map(str.strip, d.css("li::text").extract()),)) for d in detalle.css("#specs li")])}
+            yield data
+        if not detalles and not got_link:
+            self.crawler.engine.close_spider(self, "nothing to do")
+    def close(self, reason):
+        print(f"close reason:{reason}")
+        self.memory.to_json(f"data/{self.name}_memory.json")
